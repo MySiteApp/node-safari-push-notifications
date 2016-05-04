f926286a0404d57233279f47481a95dbfb48be72
Marius Gebhardt <marius.gebhardt@gmail.com>
2016-05-04 15:00:36 +0200

    Use JSZip instead of AdmZip, make 'generatePackage' async, allow object for 'iconDir'

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var utils = require("./utils"),
-    pkcs7 = require("bindings")("pkcs7"),
-    AdmZip = require('adm-zip');
+var fs = require('fs'),
+    path = require('path'),
+    utils = require("./utils"),
+    pkcs7 = require('bindings')('pkcs7'),
+    JSZip = require('jszip');
 
 var push = module.exports = {},
     PKCS7_CONTENT_REGEX = /Content-Disposition:[^\n]+\s*?([A-Za-z0-9+=/\r\n]+)\s*?-----/;
 
-push.websiteJSON = function (name, pushId, allowedDomains, urlFormattingString, authToken, webServiceURL) {
+push.websiteJSON = function(name, pushId, allowedDomains, urlFormattingString, authToken, webServiceURL) {
     return {
         "websiteName": name,
         "websitePushID": pushId,
@@ -16,32 +18,48 @@ push.websiteJSON = function (name, pushId, allowedDomains, urlFormattingString, 
     };
 };
 
-push.generatePackage = function(websiteJSON, iconsDir, certData, pKeyData, intermediate) {
-    if (typeof websiteJSON !== 'object' && !('websitePushID' in websiteJSON)) {
-        throw new Error("websiteJSON should be generated using websiteJSON() method");
+push.generatePackage = function(websiteJSON, iconsDir, certData, pKeyData, intermediate, callback) {
+    if (typeof callback == 'undefined') {
+        throw new Error('this function needs an asynchronous callback')
     }
-    var pkg = new AdmZip(),
+    if (typeof websiteJSON !== 'object' && !('websitePushID' in websiteJSON)) {
+        throw new Error('websiteJSON should be generated using websiteJSON() method');
+    }
+    var zip = new JSZip(),
         manifest = {};
 
     // Replace addFile method, to calculate the sha1 on every file
-    var _addFile = pkg.addFile;
-    pkg.addFile = function (entryName, content/** Other params omitted */) {
-        if (entryName.indexOf(".DS_Store") != -1) {
-            return;
-        }
-        manifest[entryName] = utils.sha1(content);
-        _addFile.apply(pkg, Array.prototype.splice.call(arguments, 0));
+    var _addFile = zip.file;
+    zip.file = function(name, data, o) {
+        manifest[name] = utils.sha1(data);
+        _addFile.apply(zip, Array.prototype.splice.call(arguments, 0));
     };
 
     // website.json
-    pkg.addFile("website.json", new Buffer(JSON.stringify(websiteJSON)));
+    zip.file('website.json', JSON.stringify(websiteJSON));
 
     // icon.iconset
-    pkg.addLocalFolder(iconsDir, "icon.iconset");
+    var icons = zip.folder('icon.iconset');
+    if (typeof iconsDir == 'string') {
+        // expect directory name
+        fs.readdirSync(iconsDir).forEach(function (name) {
+            var filePath = path.join(iconsDir, name);
+            if (fs.statSync(filePath).isFile()) {
+                icons.file(name, fs.readFileSync(filePath));
+            }
+        });
+    } else if (typeof iconsDir == 'object') {
+        // expect object containing file buffers
+        Object.keys(iconsDir).forEach(function(name, i) {
+            icons.file(name, iconsDir[i]);
+        });
+    } else {
+        throw new Error('iconsDir not recognized');
+    }
+    icons.file();
 
     // manifest.json
-    var manifestContent = new Buffer(JSON.stringify(manifest));
-    pkg.addFile("manifest.json", manifestContent);
+    zip.file('manifest.json', JSON.stringify(manifest));
 
     // signature
     if (typeof certData == 'string') {
@@ -57,7 +75,9 @@ push.generatePackage = function(websiteJSON, iconsDir, certData, pKeyData, inter
         content = PKCS7_CONTENT_REGEX.exec(pkcs7sig.toString());
 
     content = new Buffer(content[1], 'base64');
-    pkg.addFile("signature", content);
+    zip.file('signature', content);
 
-    return pkg.toBuffer();
+    zip.generateAsync({ type: 'nodebuffer '}).then(function(buffer) {
+        callback(buffer);
+    });
 };
